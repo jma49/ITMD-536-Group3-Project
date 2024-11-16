bf9426e0fb4c4f023bfcf4c8335384fef5bf64d7
jma49 <majincheng990128@gmail.com>
2024-11-16 14:53:18 -0600

    feat: Update next.config to make sure the env was right

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    API_KEY: process.env.API_KEY, // 显式声明环境变量
+  },
 };
 
 export default nextConfig;
